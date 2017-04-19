b3f8acef65171429524b51e2e62038418cf4c513
Elio de Berardinis <eliodeb@gmail.com>
2017-04-19 01:38:53 +0100

    implementing drop zones

--- Assets/3-Scripts/Card.cs
+++ Assets/3-Scripts/Card.cs
@@ -5,16 +5,23 @@ using UnityEngine.EventSystems; //Used to implement the Object Drag and Drop Int
 
 public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public Transform parentToReturnTo = null;
+
     //Checking when starting to drag
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("BeginDrag");
+
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     //Checking when dragging
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("Dragging");
+
         this.transform.position = eventData.position; //Position where the mouse/finger is
     }
 
@@ -22,6 +29,9 @@ public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("EndDrag");
+
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 
 }
---
+++ Assets/3-Scripts/DropZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; //Used to implement the Object Drag and Drop Interfaces
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("Exit");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + "Placed on: " + gameObject.name);
+
+        Card card = eventData.pointerDrag.GetComponent<Card>();
+        if (card != null)
+        {
+            card.parentToReturnTo = this.transform; //On drop fires before end drag so I can override Parent to return to
+        }
+    }
+
+}
