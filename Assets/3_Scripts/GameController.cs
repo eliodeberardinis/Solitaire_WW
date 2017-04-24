efac257c04991944672d248fd6f55d54dc5ddd06
Elio de Berardinis <eliodeb@gmail.com>
2017-04-24 02:41:57 +0100

    fixed problems. Cards now can be dragged around

--- Assets/3_Scripts/GameController.cs
+++ Assets/3_Scripts/GameController.cs
@@ -28,6 +28,16 @@ public class GameController : MonoBehaviour, IPointerClickHandler {
         }
 
         ShuffleMazzo();
+
+        //For Debug Purposes
+        valoriMazzo[0] = 1;
+        valoriMazzo[2] = 15 + 13;
+        valoriMazzo[5] = 3;
+        valoriMazzo[9] = 17 + 13;
+        valoriMazzo[14] = 5;
+        valoriMazzo[20] = 19 + 13;
+        valoriMazzo[27] = 7;
+
         distributeCards();
     }
 
--- Assets/3_Scripts/TablePilesDrop.cs
+++ Assets/3_Scripts/TablePilesDrop.cs
@@ -29,7 +29,7 @@ public class TablePilesDrop : MonoBehaviour, IDropHandler, IPointerEnterHandler,
         Card card = eventData.pointerDrag.GetComponent<Card>();
         if (card != null)
         {
-            if ((thisColor != card.thisColor && card.value == currentValue - 1)) //just for checking reasons. change it
+            if ((thisColor != card.thisColor && card.value == currentValue - 1)) //Change it to only allow when there are not covered cards
             {
                 //Here get the reference to the previous parent and if it's a tablepile update the value. if it's the discard pile update that
                 if (card.parentToReturnTo.gameObject.tag == "FrontPile")
@@ -61,28 +61,57 @@ public class TablePilesDrop : MonoBehaviour, IDropHandler, IPointerEnterHandler,
 
                     oldPile.thisPileList.Remove(card.gameObject);
                     //Remove also all the other cards going wih this one
-                }
 
-                Transform draggingItemTransform = card.gameObject.transform.parent;
-                card.parentToReturnTo = this.transform; //On drop fires before end drag so I can override Parent to return to
-                Card lastCard = card;
 
-                if (draggingItemTransform.childCount != 0)
-                {
-                    for (int i = 1; i < draggingItemTransform.childCount; ++i)
+                    Transform draggingItemTransform = card.gameObject.transform.parent;
+                    card.parentToReturnTo = this.transform; //On drop fires before end drag so I can override Parent to return to
+                    Card lastCard = card;
+                    thisPileList.Add(card.gameObject);
+
+                    //You can also use the old table pile list insted of the children of the dragging item (cleaner, change later)
+                    if (draggingItemTransform.childCount > 1)
                     {
-                        draggingItemTransform.GetChild(0).gameObject.GetComponent<Card>().parentToReturnTo = this.transform;
-                        lastCard = draggingItemTransform.GetChild(0).gameObject.GetComponent<Card>();
-                        thisPileList.Add(lastCard.gameObject);
+                        for (int i = 1; i < draggingItemTransform.childCount; ++i)
+                        {
+                            draggingItemTransform.GetChild(i).gameObject.GetComponent<Card>().parentToReturnTo = this.transform;
+                            lastCard = draggingItemTransform.GetChild(i).gameObject.GetComponent<Card>();
+                            oldPile.thisPileList.Remove(lastCard.gameObject);
+                            thisPileList.Add(lastCard.gameObject);
+                        }
                     }
+
+                    currentValue = lastCard.value;
+                    thisColor = lastCard.thisColor;
+                    thisSeme = lastCard.thisSeme;
+
+                    if (oldPile.thisPileList.Count == 0) //If the list only has 1 card (the currently dragged one)
+                    {
+                        //Check if the back cards have any cards left (MODIFY THIS)
+                        if (false)
+                        {
+
+                        }
+
+                        else
+                        {
+                            oldPile.currentValue = 14;
+                            oldPile.thisColor = Card.Color.NEUTRAL_COLOR;
+                            oldPile.thisSeme = Card.Seme.NEUTRAL_SEME;
+                        }
+                    }
+                    //Give the same value of the next card in the list
+                    else if(oldPile.thisPileList.Count != 0)
+                    {
+                        Card nextCardInList = oldPile.thisPileList[oldPile.thisPileList.Count - 1].GetComponent<Card>();
+                        oldPile.currentValue = nextCardInList.value;
+                        oldPile.thisColor = nextCardInList.thisColor; ;
+                        oldPile.thisSeme = nextCardInList.thisSeme;
+                    }
+
+                    //thisPileList.Add(card.gameObject);
                 }
 
-                currentValue = lastCard.value;
-                thisColor = lastCard.thisColor;
-                thisSeme = lastCard.thisSeme;
-                thisPileList.Add(card.gameObject);
-
-                //add also all the other cards going with this one and set the seme of the pile to the last one
+                else { }//if it's a discard pile
             }
         }
     }
