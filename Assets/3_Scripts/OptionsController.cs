48a5dc223f2b442c0469f32e6bc1e235df1d8955
Elio de Berardinis <eliodeb@gmail.com>
2017-04-26 16:43:54 +0100

    safety check

--- Assets/3_Scripts/OptionsController.cs
+++ Assets/3_Scripts/OptionsController.cs
@@ -34,6 +34,10 @@ public class OptionsController : MonoBehaviour {
 
     public void PauseGame()
     {
-        optionScreen.SetActive(true);
+        if (!Card.isFlippingOn && !GameController.isTranslationOn && !Card.isDragging)
+        {
+            optionScreen.SetActive(true);
+        }
+               
     }
 }
